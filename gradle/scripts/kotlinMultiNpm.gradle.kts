7d00a6b6c02856d38d5eb8ea1b03e9b3b222bc86
sarah buisson <sarahbuisson@gmail.com>
2020-04-23 00:29:25 +0200

    update version of install-jar-dependency

--- gradle/scripts/kotlinMultiNpm.gradle.kts
+++ gradle/scripts/kotlinMultiNpm.gradle.kts
@@ -59,7 +59,7 @@ class NpmToMavenPlugin : Plugin<Project> {
                         ),
                         "jarDependencies" to mutableMapOf<String, Any?>(),
                         "mavenDependencies" to mavenDependencies,
-                        "devDependencies" to mutableMapOf<String, Any?>("install-jar-dependency" to "0.0.6"),
+                        "devDependencies" to mutableMapOf<String, Any?>("install-jar-dependency" to "0.0.7"),
                         "workspaces" to mutableListOf<Any?>(),
                         "bundledDependencies" to mutableListOf<Any?>(),
                         "main" to mainJs,
